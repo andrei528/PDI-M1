535c532bce072cb711d7b8ff4158e8324fd80f3b
Daniel Gonçalves <146977180+eldanielG@users.noreply.github.com>
2026-04-12 19:34:25 -0300

    ci: adiciona workflow de validacao com testes e lint
    Cria o workflow do GitHub Actions para instalar dependências, executar flake8 e rodar pytest. Também adiciona testes básicos, inclui scikit-image no requirements e ajusta o código para melhorar a validação no CI.

--- main.py
+++ main.py
@@ -1,57 +1,53 @@
 import cv2
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 from skimage.util import random_noise
 
 from grayscale import grayscale
 from histograma import histograma
 
 
-# Carregando as imagens
-img_blood = cv2.imread("Images/imagem_Blood.png")
-img_blood = cv2.resize(img_blood, (256, 256))
+def load_and_prepare_images():
+    img_blood = cv2.imread("Images/imagem_Blood.png")
+    img_blood = cv2.resize(img_blood, (256, 256))
 
-img_path = cv2.imread("Images/imagem_Path.png")
-img_path = cv2.resize(img_path, (256, 256))
+    img_path = cv2.imread("Images/imagem_Path.png")
+    img_path = cv2.resize(img_path, (256, 256))
 
-img_retina = cv2.imread("Images/imagem_Retina.png")
-img_retina = cv2.resize(img_retina, (256, 256))
+    img_retina = cv2.imread("Images/imagem_Retina.png")
+    img_retina = cv2.resize(img_retina, (256, 256))
 
-# Transformando em escala de cinza
-img_grayscale_blood = grayscale(img_blood)
+    return img_blood, img_path, img_retina
 
-img_grayscale_path = grayscale(img_path)
 
-img_grayscale_retina = grayscale(img_retina)
+def add_salt_and_pepper_noise(image):
+    noisy_image = random_noise(image / 255, mode="s&p")
+    noisy_image = noisy_image * 255
+    return np.array(noisy_image, dtype=np.uint8)
 
-# Aplicando ruído Sal e Pimenta
 
-img_blood_ruido = random_noise(img_grayscale_blood/255, mode="s&p")
-img_blood_ruido = img_blood_ruido * 255
-img_blood_ruido = np.array(img_blood_ruido, dtype=np.uint8)
+def plot_histogram(image):
+    histogram = histograma(image)
+    x = np.linspace(0, 255, 256)
+    plt.bar(x, histogram)
+    plt.xlabel("intensidade")
+    plt.ylabel("frequencia")
+    plt.show()
 
-img_path_ruido = random_noise(img_grayscale_path/255, mode="s&p")
-img_path_ruido = img_path_ruido * 255
-img_path_ruido = np.array(img_path_ruido, dtype=np.uint8)
 
-img_retina_ruido = random_noise(img_grayscale_retina/255, mode="s&p")
-img_retina_ruido = img_retina_ruido * 255
-img_retina_ruido = np.array(img_retina_ruido, dtype=np.uint8)
+def main():
+    img_blood, img_path, img_retina = load_and_prepare_images()
 
-# Mostrando as imagens
-#cv2.imshow('blood', img_grayscale_blood)
-#cv2.imshow('path', img_grayscale_path)
-#cv2.imshow('retina', img_grayscale_retina)
-#cv2.imshow('bloodRuido', img_blood_ruido)
-#cv2.imshow('pathRuido', img_path_ruido)
-#cv2.imshow('retinaRuido', img_retina_ruido)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+    img_grayscale_blood = grayscale(img_blood)
+    img_grayscale_path = grayscale(img_path)
+    img_grayscale_retina = grayscale(img_retina)
 
-histogram = histograma(img_blood_ruido)
+    img_blood_ruido = add_salt_and_pepper_noise(img_grayscale_blood)
+    add_salt_and_pepper_noise(img_grayscale_path)
+    add_salt_and_pepper_noise(img_grayscale_retina)
 
-x = np.linspace(0, 255, 256)
-plt.bar(x, histogram)
-plt.xlabel('intensidade');
-plt.ylabel('frequência');
-plt.show()
+    plot_histogram(img_blood_ruido)
+
+
+if __name__ == "__main__":
+    main()
